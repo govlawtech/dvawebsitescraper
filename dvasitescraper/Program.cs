4dd236b4267507d5974b6abab57611596fb75e2f
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2017-05-07 11:13:32 +1000

    scraper to extract factsheet links working

---
+++ dvasitescraper/Program.cs
@@ -0,0 +1,31 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DvaSiteScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HtmlDocument factSheetByKeywordIndexPage = Scraper.FetchHtmlDocument(new Uri(Properties.Settings.Default.factListByKeyWordPageUrl));
+
+            var keywords = factSheetByKeywordIndexPage.DocumentNode.SelectNodes("//*[@id='block-views-factsheets-by-keyword-block']/div/div/div/div/h3/a").Select(n => n.InnerText);
+            var factSheetNodes = factSheetByKeywordIndexPage.DocumentNode.SelectNodes("//*[@id='block-views-factsheets-by-keyword-block']/div/div/div/div/ul/li/div/span/a");
+            var factSheetsGroupedByKeyword = from fn in factSheetNodes
+                                             let divForGroupOfFactSheets = fn.Ancestors().First(a => a.GetAttributeValue("class", "") == "item-list")
+                                             let keyWordForGroup = divForGroupOfFactSheets.SelectSingleNode("h3/a").InnerText
+                                             select new
+                                             {
+                                                 KeyWord = keyWordForGroup,
+                                                 FactSheetName = fn.InnerText,
+                                                 FactSheetRelativeLink = fn.GetAttributeValue("href","")
+                                             };
+            
+        }
+    }
+}
---
+++ dvasitescraper/Scraper.cs
@@ -0,0 +1,44 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DvaSiteScraper
+{
+    class Scraper
+    {
+        public static HtmlDocument FetchHtmlDocument(Uri uri)
+        {
+            var req = CreateWebRequest(uri);
+            Trace.WriteLine($"Sending request for: {uri.ToString()}...");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            try
+            {
+                var resp = (HttpWebResponse)req.GetResponse();
+                sw.Stop();
+                System.Diagnostics.Trace.WriteLine($"Received response in {sw.ElapsedMilliseconds} ms.");
+                var respStream = resp.GetResponseStream();
+                HtmlDocument htmlDocument = new HtmlDocument();
+                htmlDocument.Load(respStream,Encoding.UTF8);
+                return htmlDocument;
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Trace.WriteLine($"Exception for id:  {uri.ToString()}: {e.Message}");
+                return null;
+            }
+        }
+
+        private static HttpWebRequest CreateWebRequest(Uri uri)
+        {
+            var req = HttpWebRequest.Create(uri);
+            req.Timeout = 10000;
+            return (HttpWebRequest)req;
+        }
+    }
+}
