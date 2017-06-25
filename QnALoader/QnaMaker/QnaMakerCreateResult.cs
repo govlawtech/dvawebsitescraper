4dcc753d2a399d82d553d8fec8bef3732f87dbae
cchew <ching.chew@gmail.com>
2017-06-25 21:12:46 +1000

    Added code to load MRCA Factsheets into QnA Maker

---
+++ QnALoader/QnaMaker/QnaMakerCreateResult.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace QnALoader.QnaMaker
+{
+    /// <summary>
+    /// Class for QnA Maker Create response.
+    /// 
+    /// See https://westus.dev.cognitive.microsoft.com/docs/services/58994a073d9e04097c7ba6fe/operations/58994a073d9e041ad42d9baa
+    /// </summary>
+    class QnaMakerCreateResult
+    {
+        /// <summary>
+        /// The created Knowledge Base ID
+        /// </summary>
+        [JsonProperty(PropertyName = "kbId")]
+        public string KBID { get; set; }
+    }
+}
